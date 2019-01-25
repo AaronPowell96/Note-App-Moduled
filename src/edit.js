97265da35790efabe33d2ea083b1f0e8251d2021
Aaron <Aaronpowell829@gmail.com>
2019-01-25 23:11:43 +0000

    refactored edit module

--- src/edit.js
+++ src/edit.js
@@ -1,1 +1,40 @@
-console.log("edit.js")
+import { initEditPage, generateLastEdited } from "./views"
+import { updateNote, removeNote } from "./notes";
+
+const noteTitle = document.querySelector("#noteTitle");
+const noteBody = document.querySelector("#noteBody");
+const noteUpdated = document.querySelector("#updatedInfo");
+const removeEl = document.querySelector("#removeNote");
+const noteId = location.hash.substring(1);
+
+initEditPage(noteId)
+
+noteTitle.addEventListener("input", e => {
+    const note = updateNote(noteId, {
+        title: e.target.value
+    })
+    noteUpdated.textContent = generateLastEdited(note.updatedAt);
+});
+
+noteBody.addEventListener("input", e => {
+
+    const note = updateNote(noteId, {
+        body: e.target.value
+    })
+    noteUpdated.textContent = generateLastEdited(note.updatedAt);
+});
+
+removeEl.addEventListener("click", () => {
+    removeNote(noteId);
+    location.assign(`/index.html`);
+});
+
+document.querySelector("#backButton").addEventListener("click", () => {
+    location.assign(`/index.html`);
+});
+
+window.addEventListener("storage", e => {
+    if (e.key === "notes") {
+        initEditPage(noteId)
+    }
+});
