fbddf7e690906ee38edca7f2ea3ed424fa111969
Aaron <Aaronpowell829@gmail.com>
2019-01-25 23:11:20 +0000

    imported modules

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
 import { getNotes, createNote, removeNote } from "./notes.js";
+import { getFilters, setFilters } from "./filters"
+import { renderNotes } from "./views"
 
-// console.log(getNotes())
-// createNote();
-removeNote("");
-console.log(getNotes())
+renderNotes();
+
+document.querySelector("#createNote").addEventListener("click", e => {
+    const id = createNote()
+    location.assign(`/edit.html#${id}`);
+});
+document.querySelector("#searchNotes").addEventListener("input", e => {
+    setFilters({
+        searchText: e.target.value
+    })
+    renderNotes();
+});
+
+document.querySelector("#filterBy").addEventListener("change", e => {
+    setFilters({
+        sortBy: e.target.value
+    })
+    renderNotes();
+});
+
+window.addEventListener("storage", e => {
+    if (e.key === "notes") {
+        renderNotes();
+    }
+});
